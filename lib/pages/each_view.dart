a4fd9c24c2f6087b1a6bc641e4955975ff786bb3
dengjian <https://wubo.coding.net/signin>
2020-09-25 16:56:33 +0800

    feat: 波浪形贝塞尔曲线

--- lib/pages/each_view.dart
+++ lib/pages/each_view.dart
@@ -12,12 +12,51 @@ class _EachViewState extends State<EachView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget._title),
-      ),
-      body: Center(
-        child: Text(widget._title),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(widget._title),
+        ),
+        body: Column(
+          children: [
+            ClipPath(
+              clipper: BottomClipper(),
+              child: Container(
+                color: Colors.lightBlueAccent,
+                height: 200,
+              ),
+            ),
+            Center(
+              child: Text(widget._title),
+            ),
+          ],
+        ));
+  }
+}
+
+class BottomClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height - 20);
+    var firstControlPoint = Offset(size.width / 4, size.height);
+    var firstEndPoint = Offset(size.width / 2.25, size.height - 30);
+
+    path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
+        firstEndPoint.dx, firstEndPoint.dy);
+
+    var secondControlPoint = Offset(size.width / 4 * 3, size.height - 80);
+    var secondEndPoint = Offset(size.width, size.height - 40);
+
+    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
+        secondEndPoint.dx, secondEndPoint.dy);
+
+    path.lineTo(size.width, size.height - 40);
+    path.lineTo(size.width, 0);
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
   }
 }
