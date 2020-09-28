8839897e93d7c11741d5ca56aa2c6ed04a3a9851
dengjian <https://wubo.coding.net/signin>
2020-09-28 17:29:49 +0800

    feat: add home background image

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,52 +10,65 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     data = data.isNotEmpty ? data : ModalRoute.of(context).settings.arguments;
+
+    //set background image
+    String bgImage = data['isDaytime'] ? 'day.png' : 'night.png';
+    Color bgColor = data['isDaytime'] ? Colors.blue : Colors.indigo[700];
+
     print(data);
     return Scaffold(
+      backgroundColor: bgColor,
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(0, 120.0, 0, 0),
-          child: Column(
-            children: [
-              FlatButton.icon(
-                  onPressed: () async {
-                    dynamic res =
-                        await Navigator.pushNamed(context, '/location');
-                    if (res != null) {
-                      setState(() {
-                        data = {
-                          'time': res['time'],
-                          'location': res['location'],
-                          'isDaytime': res['isDaytime'],
-                          'flag': res['flay'],
-                        };
-                      });
-                    }
-                  },
-                  icon: Icon(Icons.edit_location),
-                  label: Text('Edit Location')),
-              SizedBox(
-                height: 20.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    data['location'],
-                    style: TextStyle(fontSize: 28.0, letterSpacing: 2.0),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                data['time'],
-                style: TextStyle(
-                  fontSize: 66.0,
+        child: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage('assets/$bgImage'),
+            fit: BoxFit.cover,
+          )),
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+            child: Column(
+              children: [
+                FlatButton.icon(
+                    onPressed: () async {
+                      dynamic res =
+                          await Navigator.pushNamed(context, '/location');
+                      if (res != null) {
+                        setState(() {
+                          data = {
+                            'time': res['time'],
+                            'location': res['location'],
+                            'isDaytime': res['isDaytime'],
+                            'flag': res['flay'],
+                          };
+                        });
+                      }
+                    },
+                    icon: Icon(Icons.edit_location),
+                    label: Text('Edit Location')),
+                SizedBox(
+                  height: 20.0,
                 ),
-              )
-            ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      data['location'],
+                      style: TextStyle(fontSize: 28.0, letterSpacing: 2.0),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  data['time'],
+                  style: TextStyle(
+                    fontSize: 66.0,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
