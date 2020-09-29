dbe038b1633f6aeaa3596a6c10d2c3e66b582ade
dengjian <https://wubo.coding.net/signin>
2020-09-29 16:39:36 +0800

    feat: myRaisedButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:my_flutter_app/pages/choose_location.dart';
-import 'package:my_flutter_app/pages/home.dart';
-import 'package:my_flutter_app/pages/loading.dart';
 
-void main() => runApp(MaterialApp(
-        initialRoute: '/',
-        debugShowCheckedModeBanner: false,
-        routes: {
-          '/': (context) => Loading(),
-          '/home': (context) => Home(),
-          '/location': (context) => ChooseLocation()
-        }));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final appTitle = 'Drawer Demo';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: Center(child: MyButton()),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        final snackBar = SnackBar(
+          content: Text('you clicked'),
+        );
+        Scaffold.of(context).showSnackBar(snackBar);
+      },
+      child: Container(
+        padding: EdgeInsets.all(12.0),
+        decoration: BoxDecoration(
+            color: Theme.of(context).buttonColor,
+            borderRadius: BorderRadius.circular(8.0)),
+        child: Text('My button'),
+      ),
+    );
+  }
+}
