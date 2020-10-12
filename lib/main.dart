82fb62093898ef6ee395606609df110d82126505
dengjian <https://wubo.coding.net/signin>
2020-10-12 10:45:39 +0800

    animate container

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,73 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp(
-      items: List<String>.generate(20, (index) => "item ${index + 1}"),
-    ));
+void main() => runApp(AnimatedContainerApp());
 
-class MyApp extends StatelessWidget {
-  final List<String> items;
+class AnimatedContainerApp extends StatefulWidget {
+  @override
+  _AnimatedContainerAppState createState() => _AnimatedContainerAppState();
+}
 
-  MyApp({Key key, @required this.items}) : super(key: key);
+class _AnimatedContainerAppState extends State<AnimatedContainerApp> {
+  // Define the various properties with default values. Update these properties
+  // when the user taps a FloatingActionButton.
+  double _width = 50;
+  double _height = 50;
+  Color _color = Colors.green;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8);
 
   @override
   Widget build(BuildContext context) {
-    final title = 'Dissmsing time';
     return MaterialApp(
-      title: title,
       home: Scaffold(
-          appBar: AppBar(
-            title: Text(title),
+        appBar: AppBar(
+          title: Text('AnimatedContainer Demo'),
+        ),
+        body: Center(
+          child: AnimatedContainer(
+            // Use the properties stored in the State class.
+            width: _width,
+            height: _height,
+            decoration: BoxDecoration(
+              color: _color,
+              borderRadius: _borderRadius,
+            ),
+            // Define how long the animation should take.
+            duration: Duration(seconds: 1),
+            // Provide an optional curve to make the animation feel smoother.
+            curve: Curves.fastOutSlowIn,
+            child: Center(child: Text('$_width', style: TextStyle(color: Colors.white)),),
           ),
-          body: ListView.builder(
-            itemCount: items.length,
-            itemBuilder: (context, index) {
-              final item = items[index];
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.play_arrow),
+          // When the user taps the button
+          onPressed: () {
+            // Use setState to rebuild the widget with new values.
+            setState(() {
+              // Create a random number generator.
+              final random = Random();
 
-              return Dismissible(
-                key: Key(item),
-                onDismissed: (direction) {
-                  items.removeAt(index);
-                  Scaffold.of(context).showSnackBar(SnackBar(
-                    content: Text('$item dismissed'),
-                  ));
-                },
-                background: Container(color: Colors.red[300],),
-                child: ListTile(title: Text('$item')),
+              // Generate a random width and height.
+              _width = random.nextInt(300).toDouble();
+              _height = random.nextInt(300).toDouble();
+
+              // Generate a random color.
+              _color = Color.fromRGBO(
+                random.nextInt(256),
+                random.nextInt(256),
+                random.nextInt(256),
+                1,
               );
-            },
-          )),
+
+              // Generate a random border radius.
+              _borderRadius =
+                  BorderRadius.circular(random.nextInt(100).toDouble());
+            });
+          },
+        ),
+      ),
     );
   }
 }
