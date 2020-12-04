cafd73d6373dcf08dd9f420992f310afa601f5c9
dengjian <https://wubo.coding.net/signin>
2020-12-04 15:07:47 +0800

    feat: add Animated-Opacity, -List, -Padding, -PhysicalModel

--- lib/pages/animations.dart
+++ lib/pages/animations.dart
@@ -16,6 +16,8 @@ class _AnimationPageState extends State<AnimationPage> {
   bool _selected = false;
   double _fontSize = 30.0;
   Color _color = Colors.blue[500];
+  final listKey = GlobalKey<AnimatedListState>();
+  List<String> _items = ['😀', '😎'];
 
   Widget _rectContainer() {
     return AnimatedContainer(
@@ -54,6 +56,66 @@ class _AnimationPageState extends State<AnimationPage> {
     );
   }
 
+  Widget _animatedList() {
+    return AnimatedList(
+      key: listKey,
+      initialItemCount: _items.length,
+      itemBuilder: (context, index, animation) {
+        return _buildItem(context, index, animation);
+      },
+    );
+  }
+
+  Widget _buildItem(context, index, animation) {
+    String item = _items[index];
+    return SlideTransition(
+      child: ListTile(title: Text(item)),
+      position: Tween<Offset>(begin: Offset(-1, 0), end: Offset.zero)
+          .animate(animation),
+    );
+  }
+
+  // animated opacity
+  Widget _opacity() {
+    return AnimatedOpacity(
+      opacity: _selected ? 0.0 : 1.0,
+      duration: Duration(seconds: 2),
+      child: FlutterLogo(size: 50),
+    );
+  }
+
+  // animated padding
+  Widget _padding() {
+    return AnimatedPadding(
+      padding: _selected
+          ? EdgeInsets.only(top: 100, bottom: 100)
+          : EdgeInsets.only(left: 100, right: 100),
+      curve: Curves.bounceOut,
+      duration: Duration(seconds: 2),
+      child: Container(
+        color: Colors.blueAccent,
+      ),
+    );
+  }
+
+  Widget _model() {
+    return AnimatedPhysicalModel(
+      duration: Duration(milliseconds: 600),
+      curve: Curves.easeInOutBack,
+      shape: BoxShape.rectangle,
+      shadowColor: Colors.black,
+      color: Colors.white,
+      elevation: _selected ? 0.0 : 6.0,
+      borderRadius: _selected ? BorderRadius.all(Radius.circular(0)) : BorderRadius.all(Radius.circular(10)),
+      child: Container(
+        height: 120,
+        width: 120,
+        color: Colors.blue[50],
+        child: FlutterLogo(size:50),
+      ),
+    );
+  }
+
   void _switchBorderSize() {
     setState(() {
       _selected = !_selected;
@@ -72,6 +134,13 @@ class _AnimationPageState extends State<AnimationPage> {
         _selected = !_selected;
       });
     });
+    bus.on('addList', (arg) {
+      setState(() {
+        listKey.currentState
+            .insertItem(0, duration: Duration(milliseconds: 500));
+        _items.add('😂');
+      });
+    });
     super.initState();
   }
 
@@ -85,7 +154,12 @@ class _AnimationPageState extends State<AnimationPage> {
           _gridItemContainer('AnimatedBuilder', SpinConatiner()),
           _gridItemContainer('AnimatedContainer', _rectContainer(), 'expand'),
           _gridItemContainer('AnimatedCrossFade', _cross(), 'expand'),
-          _gridItemContainer('AnimatedDefaultTextStyle', _defaultText(), 'expose'),
+          _gridItemContainer(
+              'AnimatedDefaultTextStyle', _defaultText(), 'expose'),
+          _gridItemContainer('AnimatedList', _animatedList(), 'addList'),
+          _gridItemContainer('AnimatedOpacity', _opacity(), 'expand'),
+          _gridItemContainer('AnimatedPadding', _padding(), 'expand'),
+          _gridItemContainer('AnimatedPhysicalModel', _model(), 'expand'),
         ],
       ),
     );
